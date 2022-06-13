2d460e8eeddcd1dff8d4cf145d8c7966ce23cff8
Kevin Herrera <kevin@herrera.io>
2022-06-13 04:20:16 +0000

    Defining colors.

---
+++ src/common.test.ts
@@ -0,0 +1,28 @@
+import { Color } from "./common";
+
+describe("Color", () => {
+  test.each([
+    ["BerryRed", 30],
+    ["Red", 31],
+    ["Orange", 32],
+    ["Yellow", 33],
+    ["OliveGreen", 34],
+    ["LimeGreen", 35],
+    ["Green", 36],
+    ["MintGreen", 37],
+    ["Teal", 38],
+    ["SkyBlue", 39],
+    ["LightBlue", 40],
+    ["Blue", 41],
+    ["Grape", 42],
+    ["Violet", 43],
+    ["Lavender", 44],
+    ["Magenta", 45],
+    ["Salmon", 46],
+    ["Charcoal", 47],
+    ["Grey", 48],
+    ["Taupe", 49],
+  ])("verify that the name %s matches the ID %d", (name, id) => {
+    expect(Color[name]).toEqual(id);
+  });
+});
---
+++ src/common.ts
@@ -0,0 +1,106 @@
+/**
+ * The possible colors in Todoist.
+ *
+ * {@link https://developer.todoist.com/guides/#colors Colors API}
+ */
+export enum Color {
+  /**
+   * #b8256f
+   */
+  BerryRed = 30,
+
+  /**
+   * #db4035
+   */
+  Red,
+
+  /**
+   * #ff9933
+   */
+  Orange,
+
+  /**
+   * #fad000
+   */
+  Yellow,
+
+  /**
+   * #afb83b
+   */
+  OliveGreen,
+
+  /**
+   * #7ecc49
+   */
+  LimeGreen,
+
+  /**
+   * #299438
+   */
+  Green,
+
+  /**
+   * #6accbc
+   */
+  MintGreen,
+
+  /**
+   * #158fad
+   */
+  Teal,
+
+  /**
+   * #14aaf5
+   */
+  SkyBlue,
+
+  /**
+   * #96c3eb
+   */
+  LightBlue,
+
+  /**
+   * #4073ff
+   */
+  Blue,
+
+  /**
+   * #884dff
+   */
+  Grape,
+
+  /**
+   * #af38eb
+   */
+  Violet,
+
+  /**
+   * #eb96eb
+   */
+  Lavender,
+
+  /**
+   * #e05194
+   */
+  Magenta,
+
+  /**
+   * #ff8d85
+   */
+  Salmon,
+
+  /**
+   * #808080
+   */
+  Charcoal,
+
+  /**
+   * #b8b8b8
+   */
+  Grey,
+
+  /**
+   * #ccac93
+   */
+  Taupe,
+}
