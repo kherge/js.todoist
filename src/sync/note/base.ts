ddc49086b64b624a61994aecc4a2e0b5747bdd3a
Kevin Herrera <kevin@herrera.io>
2022-06-14 23:50:09 +0000

    Updating the models using real data.

--- src/sync/note/base.ts
+++ src/sync/note/base.ts
@@ -1,11 +1,11 @@
 import { State } from "../common";
 
 /**
- * The sync jSON model for a [file attachment][doc].
+ * The sync JSON model for a [file attachment][doc].
  *
  * [doc]: https://developer.todoist.com/sync/v8/#file-attachments
  */
-export interface FileAttachment {
+export interface Attachment {
   /**
    * The name of the file.
    */
@@ -24,20 +24,50 @@ export interface FileAttachment {
   /**
    * The download URL for the file.
    */
-  file_url: "completed" | "pending";
+  file_url: string;
 
   /**
-   * TODO
+   * The resource type.
    */
-  upload_state: string;
+  resource_type: string;
+
+  /**
+   * The upload state.
+   */
+  upload_state: "completed" | "pending";
 }
 
 /**
- * The sync JSON model for an [item note (a.k.a comment)][doc].
+ * The sync JSON model for an image attachment.
+ */
+export interface ImageAttachment extends Attachment {
+  /**
+   * The image URL.
+   */
+  image: string;
+
+  /**
+   * The image height.
+   */
+  image_height: number;
+
+  /**
+   * The image width.
+   */
+  image_width: number;
+
+  /**
+   * The resource type.
+   */
+  resource_type: "image";
+}
+
+/**
+ * The sync JSON model for a [note (a.k.a comment)][doc].
  *
  * [doc]: https://developer.todoist.com/sync/v8/#item-notes
  */
-export interface ItemNote {
+export interface Note {
   /**
    * The Markdown formatted content of the note.
    */
@@ -46,7 +76,7 @@ export interface ItemNote {
   /**
    * The file attached to the note.
    */
-  file_attachment: FileAttachment | null;
+  file_attachment: Attachment | ImageAttachment | null;
 
   /**
    * The note ID.
@@ -59,19 +89,9 @@ export interface ItemNote {
   is_deleted: State;
 
   /**
-   * The item the note is a part of.
-   */
-  item_id: number;
-
-  /**
    * The legacy note ID.
    */
   legacy_id?: number;
-
-  /**
-   * The legacy item ID.
-   */
-  legacy_item_id?: number;
 
   /**
    * The legacy project ID.
@@ -101,61 +121,25 @@ export interface ItemNote {
   /**
    * The list of user IDs to notify.
    */
-  uids_to_notify: number[];
+  uids_to_notify: null | number[];
 }
 
 /**
- * The sync JSON model for a [project note (a.k.a comment)][doc].
- *
- * [doc]: https://developer.todoist.com/sync/v8/#project-notes
+ * The sync JSON model for an item note.
  */
-export interface ProjectNote {
+export interface ItemNote extends Note {
   /**
-   * The Markdown formatted content of the note.
+   * The item the note is a part of.
    */
-  content: string;
+  item_id: number;
 
   /**
-   * The file attached to the note.
+   * The legacy item ID.
    */
-  file_attachment: FileAttachment | null;
-
-  /**
-   * The note ID.
-   */
-  id: number;
-
-  /**
-   * The deleted state of the note.
-   */
-  is_deleted: State;
-  /**
-   * The ISO 8601 timestamp of when the note was posted.
-   */
-  posted: string;
-
-  /**
-   * The user ID of the user that posted the note.
-   */
-  posted_uid: number;
-
-  /**
-   * The project the note is a part of.
-   */
-  project_id: number;
-
-  /**
-   * The emoji reactions to the note.
-   */
-  reactions: Reactions | null;
-
-  /**
-   * The list of user IDs to notify.
-   */
-  uids_to_notify: number[];
+  legacy_item_id?: number;
 }
 
 /**
- * The reactions to a note, where the key is the emoji and the value is a list of user IDs.
+ * The emoji reactions to a note, where the key is the emoji and the value is a list of user IDs.
  */
 export type Reactions = Record<string, number[]>;
